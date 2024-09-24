b2036f8543632d74ea57a69cd77201e452b384df
zaulgin <zaulgin@mail.ru>
2024-09-25 00:57:32 +0900

    добавлен вывод ИУЛ

--- internal/utils/generatePDF.go
+++ internal/utils/generatePDF.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"UserPDFMaker/internal/data"
+	"fmt"
 	"image"
 	_ "image/jpeg"
 	"math"
@@ -13,19 +14,23 @@ import (
 )
 
 const (
-	label1   = "Наименование объекта"
-	label2   = "Номер"
-	label3   = "п/п"
-	label4   = "Обозначение"
-	label5   = "документа"
-	label6   = "Наименование документа"
-	label7   = "Номер последнего"
-	label8   = "изменения (версии)"
-	label9   = "Характер работы"
-	label10  = "Фамилия"
-	label11  = "Подпись"
-	label12  = "Дата подписания"
-	defaultX = 22.0
+	label1            = "Наименование объекта"
+	label2            = "Номер"
+	label3            = "п/п"
+	label4            = "Обозначение"
+	label5            = "документа"
+	label6            = "Наименование документа"
+	label7            = "Номер последнего"
+	label8            = "изменения (версии)"
+	label9            = "Характер работы"
+	label10           = "Фамилия"
+	label11           = "Подпись"
+	label12           = "Дата подписания"
+	defaultX          = 22.0
+	defaultY          = 10.0
+	bottomMargin      = 20.0
+	pageHeight        = 297.0
+	defaultCellHeight = 5.0
 )
 
 func GeneratePDF(input data.Input) error {
@@ -35,19 +40,17 @@ func GeneratePDF(input data.Input) error {
 	pdf.AddUTF8Font("TNRBold", "", "fonts/times_new_roman_bold.ttf")
 	pdf.SetFont("TNRBold", "", 11.5)
 
-	defaultY := 10.0
 	x := 90.0
-	y := pdf.GetY()
 	pdf.SetX(x)
 	pdf.MultiCell(108, 5, input.ObjectName, "1", "C", false)
-	y = pdf.GetY() - y
-
+	y := pdf.GetY() - defaultY
 	pdf.SetXY(defaultX, defaultY)
 	firstCellX := x - defaultX
 	pdf.CellFormat(firstCellX, y, label1, "1", 1, "C", false, 0, "")
-	pdf.SetX(defaultX)
 
 	pdf.SetFont("TNR", "", 11.5)
+
+	pdf.SetX(defaultX)
 	pdf.CellFormat(firstCellX/2, 5, label2, "LTR", 1, "C", false, 0, "")
 	pdf.SetX(defaultX)
 	pdf.CellFormat(firstCellX/2, 5, label3, "LBR", 0, "C", false, 0, "")
@@ -116,8 +119,10 @@ func GeneratePDF(input data.Input) error {
 
 	for _, user := range input.Users {
 		pdf.SetX(defaultX)
-		outputUserInfo(user, pdf.GetY(), pdf)
+		printUserInfo(user, pdf.GetY(), pdf)
 	}
+
+	printInfoCertifyingSheet(input, pdf.GetY(), pdf)
 
 	err := pdf.OutputFileAndClose("output.pdf")
 	if err != nil {
@@ -127,18 +132,79 @@ func GeneratePDF(input data.Input) error {
 	return nil
 }
 
-func outputUserInfo(user data.User, currentY float64, pdf *gofpdf.Fpdf) {
+// вывод строки с информацией об ИУЛ и количестве листов (последняя строка)
+func printInfoCertifyingSheet(input data.Input, currentY float64, pdf *gofpdf.Fpdf) {
+	maxY := 10.0
+	icsArr := pdf.SplitText(input.InfoCertifyingSheet, 75)
+	lenIcs := len(icsArr)
+
+	if lenIcs*defaultCellHeight > 10 {
+		maxY = float64(lenIcs) * defaultCellHeight
+	}
+
+	remainingSpace := pageHeight - (currentY + maxY)
+
+	// Если до конца страницы осталось <= 20, создаём новую страницу
+	if remainingSpace < bottomMargin {
+		pdf.AddPage()
+		currentY = defaultY
+	}
+
+	pdf.SetXY(defaultX+55, currentY)
+	if lenIcs <= 1 {
+		pdf.SetXY(defaultX+55, currentY+2.5)
+	}
+	pdf.MultiCell(75, 5, input.InfoCertifyingSheet, "", "CM", false)
+
+	topMargin := 0.0
+	y := pdf.GetY() - currentY
+	if y > 10 {
+		topMargin = (y - 10) / 2
+	}
+
+	pdf.SetXY(defaultX, currentY+topMargin)
+
+	pdf.CellFormat(55, 5, "Информационно-", "", 2, "CM", false, 0, "")
+	pdf.CellFormat(55, 5, "удостоверяющий лист", "", 0, "CM", false, 0, "")
+
+	pdf.SetXY(defaultX+130, currentY)
+	pdf.CellFormat(21, 5, "Лист", "1", 0, "CM", false, 0, "")
+	pdf.CellFormat(25, 5, "Листов", "1", 1, "CM", false, 0, "")
+
+	pdf.SetX(defaultX + 130)
+	pdf.CellFormat(21, 5, fmt.Sprintf("%d", input.Page), "", 0, "CM", false, 0, "")
+	pdf.CellFormat(25, 5, fmt.Sprintf("%d", input.Limit), "", 0, "CM", false, 0, "")
+
+	pdf.SetXY(defaultX, currentY)
+	pdf.CellFormat(55, maxY, "", "1", 0, "", false, 0, "")
+	pdf.CellFormat(75, maxY, "", "1", 0, "", false, 0, "")
+
+	pdf.SetXY(defaultX+130, currentY+5)
+	pdf.CellFormat(21, maxY-5, "", "1", 0, "", false, 0, "")
+	pdf.CellFormat(25, maxY-5, "", "1", 0, "", false, 0, "")
+}
+
+// вывод пользователей
+func printUserInfo(user data.User, currentY float64, pdf *gofpdf.Fpdf) {
 	workTypeArr := pdf.SplitText(user.WorkType, 46)
 	fullNameArr := pdf.SplitText(user.FullName, 46)
 	maxKoef := math.Max(float64(len(workTypeArr)), float64(len(fullNameArr)))
 
 	maxY := 10.0
-	cellHeight := 5.0
+	cellHeight := defaultCellHeight
 	if maxKoef != 0 {
 		cellHeight *= maxKoef
 	}
 	if cellHeight > maxY {
 		maxY = cellHeight
+	}
+
+	remainingSpace := pageHeight - (currentY + maxY)
+
+	// Если до конца страницы осталось <= 20, создаём новую страницу
+	if remainingSpace < bottomMargin {
+		pdf.AddPage()
+		currentY = defaultY
 	}
 
 	var y float64
